1d660d88d2a9fd9375272c229ccfe176e8018ad2
Will Christerson <wbchristerson@gmail.com>
2018-04-16 14:12:12 -0400

    chore: Remove old commented-out code

--- src/App.js
+++ src/App.js
@@ -4,35 +4,9 @@ import Map from './Map'
 import Search from './Search'
 import './App.css'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
 import escapeRegExp from 'escape-string-regexp'
 
-// Saint Petersburg: { lat: 59.95, lng: 30.33 }
-
 class App extends Component {
-  // state = {
-  //   placesList: [
-  //     { title: "Roundhouse Aquarium", location: {lat: 33.883368, lng: -118.414595} },
-  //     { title: "Becker's Bakery & Deli", location: {lat: 33.883937, lng: -118.410461} },
-  //     { title: "The Kettle", location: {lat: 33.885366, lng: -118.409663} },
-  //     { title: "California Pizza Kitchen", location: {lat: 33.899041, lng: -118.394780} },
-  //     { title: "Whole Foods", location: {lat: 33.906682, lng: -118.392783} },
-  //     { title: "Ralphs", location: {lat: 33.896281, lng: -118.394272} },
-  //     { title: "Pages Book Store", location: {lat: 33.883141, lng: -118.409656} },
-  //     { title: "Peet's Coffee & Tea", location: {lat: 33.885273, lng: -118.408892} },
-  //     { title: "Martha's 22nd Street Grill", location: {lat: 33.869321, lng: -118.403978} },
-  //     { title: "45th Street Beach", location: {lat: 33.904201, lng: -118.422161} },
-  //     { title: "Target", location: {lat: 33.888592, lng: -118.394307} },
-  //     { title: "Islands Restaurant", location: {lat: 33.899627, lng: -118.394053} },
-  //     { title: "El Sombrero", location: {lat: 33.874037, lng: -118.394050} },
-  //     { title: "Von's", location: {lat: 33.885148, lng: -118.407879} },
-  //     { title: "Barnes and Noble", location: {lat: 33.900630, lng: -118.380849} },
-  //     { title: "Mira Costa High School", location: {lat: 33.873556, lng: -118.389907} },
-  //     { title: "Manhattan Beach Library", location: {lat: 33.887115, lng: -118.410357} }
-  //   ],
-  //   placesFilter: [{ title: "Manhattan Beach Library", location: {lat: 33.887115, lng: -118.410357} }]
-  // }
-
   state = {
     placesList: [
       { title: "Roundhouse Aquarium", location: {lat: 33.883368, lng: -118.414595} },
@@ -57,15 +31,6 @@ class App extends Component {
   }
 
   // componentDidMount() {
-  //   this.setState({
-  //     placesList:
-  //   })
-  // }
-
-  // setMarkerQuery(newQuery) {
-  //   this.setState({
-  //     filterQuery: newQuery
-  //   })
   // }
 
   setMarkerQuery(newQuery) {
@@ -74,8 +39,6 @@ class App extends Component {
     })
   }
 
-  // <Search placesList={this.state.placesList}
-  //   setMarkerQuery={this.setMarkerQuery.bind(this)}/>
   render() {
     let filteredPlaces
     if (this.state.filterQuery) {
