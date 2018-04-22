4b5dc4df90755c8b17c37f54d974b0247c30330e
Will Christerson <wbchristerson@gmail.com>
2018-04-22 18:25:13 -0400

    style: Include item icons

--- src/App.js
+++ src/App.js
@@ -9,24 +9,24 @@ import escapeRegExp from 'escape-string-regexp'
 class App extends Component {
   state = {
     placesList: [
-      { title: "Roundhouse Aquarium", location: {lat: 33.883368, lng: -118.414595} },
-      { title: "Becker's Bakery & Deli", location: {lat: 33.883937, lng: -118.410461} },
-      { title: "The Kettle", location: {lat: 33.885366, lng: -118.409663} },
-      { title: "California Pizza Kitchen", location: {lat: 33.899041, lng: -118.394780} },
-      { title: "Whole Foods", location: {lat: 33.906682, lng: -118.392783} },
-      { title: "Ralphs", location: {lat: 33.896281, lng: -118.394272} },
-      { title: "Pages Book Store", location: {lat: 33.883141, lng: -118.409656} },
-      { title: "Peet's Coffee & Tea", location: {lat: 33.885273, lng: -118.408892} },
-      { title: "Martha's 22nd Street Grill", location: {lat: 33.869321, lng: -118.403978} },
-      { title: "Beach, Manhattan Beach", location: {lat: 33.887159533767874, lng: -118.41460291075771} },
-      { title: "Target", location: {lat: 33.888592, lng: -118.394307} },
-      { title: "Islands Restaurant", location: {lat: 33.899627, lng: -118.394053} },
-      { title: "El Sombrero", location: {lat: 33.874037, lng: -118.394050} },
-      { title: "Von's", location: {lat: 33.885148, lng: -118.407879} },
-      { title: "Barnes and Noble", location: {lat: 33.900680, lng: -118.380849} },
-      { title: "Mira Costa High School", location: {lat: 33.873556, lng: -118.389907} },
-      { title: "Manhattan Beach Library", location: {lat: 33.887115, lng: -118.410357} },
-      { title: "Trader Joe's", location: {lat: 33.900345, lng: -118.381278} }
+      { title: "Roundhouse Aquarium", placeType: "", location: {lat: 33.883368, lng: -118.414595} },
+      { title: "Becker's Bakery & Deli", placeType: "bakery", location: {lat: 33.883937, lng: -118.410461} },
+      { title: "The Kettle", placeType: "restaurant", location: {lat: 33.885366, lng: -118.409663} },
+      { title: "California Pizza Kitchen", placeType: "restaurant", location: {lat: 33.899041, lng: -118.394780} },
+      { title: "Whole Foods", placeType: "grocery store", location: {lat: 33.906682, lng: -118.392783} },
+      { title: "Ralphs", placeType: "grocery store", location: {lat: 33.896281, lng: -118.394272} },
+      { title: "Pages Book Store", placeType: "", location: {lat: 33.883141, lng: -118.409656} },
+      { title: "Peet's Coffee & Tea", placeType: "cafe", location: {lat: 33.885273, lng: -118.408892} },
+      { title: "Martha's 22nd Street Grill", placeType: "restaurant", location: {lat: 33.869321, lng: -118.403978} },
+      { title: "Beach, Manhattan Beach", placeType: "beach", location: {lat: 33.887159533767874, lng: -118.41460291075771} },
+      { title: "Target", placeType: "grocery store", location: {lat: 33.888592, lng: -118.394307} },
+      { title: "Islands Restaurant", placeType: "restaurant", location: {lat: 33.899627, lng: -118.394053} },
+      { title: "El Sombrero", placeType: "restaurant", location: {lat: 33.874037, lng: -118.394050} },
+      { title: "Von's", placeType: "grocery store", location: {lat: 33.885148, lng: -118.407879} },
+      { title: "Barnes and Noble", placeType: "", location: {lat: 33.900680, lng: -118.380849} },
+      { title: "Mira Costa High School", placeType: "school", location: {lat: 33.873556, lng: -118.389907} },
+      { title: "Manhattan Beach Library", placeType: "library", location: {lat: 33.887115, lng: -118.410357} },
+      { title: "Trader Joe's", placeType: "grocery store", location: {lat: 33.900345, lng: -118.381278} }
     ],
     filterQuery: '',
     imageSrc: '',
@@ -35,26 +35,26 @@ class App extends Component {
     // currentLocation: {}, // lat, lng coordinates of location
   }
 
-  componentDidMount() {
-    fetch(`https://api.foursquare.com/v2/venues/search?ll=33.888928,-118.393534&client_id=TPSVD55HZSB2CSKSFO1QITDRGGDUBXR1320V1C42EKBFC30T&client_secret=VZCPYPTDGXIBA2CJ3MQ4AU0SHRW0QOUUGYKWIXOZAZ20ID4U&v=20130815&near&query=target&limit=1`)
-    .then((res) => res.text())
-    .then((text) => {
-      let formattedResponse = JSON.parse(text).response.venues[0];
-      return formattedResponse.id
-    })
-    .then((id) => {
-      fetch(`https://api.foursquare.com/v2/venues/${id}/photos?client_id=TPSVD55HZSB2CSKSFO1QITDRGGDUBXR1320V1C42EKBFC30T&client_secret=VZCPYPTDGXIBA2CJ3MQ4AU0SHRW0QOUUGYKWIXOZAZ20ID4U&v=20130815`)
-      .then((res) => res.text())
-      .then((text) => {
-        let formattedNewResponse = JSON.parse(text)
-        let imageInfo = formattedNewResponse.response.photos.items[0]
-        this.setState({
-          imageSrc: imageInfo.prefix + '36x36' + imageInfo.suffix
-        })
-      })
-    })
-    .catch((error) => console.log("Error: ", error))
-  }
+  // componentDidMount() {
+  //   fetch(`https://api.foursquare.com/v2/venues/search?ll=33.888928,-118.393534&client_id=TPSVD55HZSB2CSKSFO1QITDRGGDUBXR1320V1C42EKBFC30T&client_secret=VZCPYPTDGXIBA2CJ3MQ4AU0SHRW0QOUUGYKWIXOZAZ20ID4U&v=20130815&near&query=target&limit=1`)
+  //   .then((res) => res.text())
+  //   .then((text) => {
+  //     let formattedResponse = JSON.parse(text).response.venues[0];
+  //     return formattedResponse.id
+  //   })
+  //   .then((id) => {
+  //     fetch(`https://api.foursquare.com/v2/venues/${id}/photos?client_id=TPSVD55HZSB2CSKSFO1QITDRGGDUBXR1320V1C42EKBFC30T&client_secret=VZCPYPTDGXIBA2CJ3MQ4AU0SHRW0QOUUGYKWIXOZAZ20ID4U&v=20130815`)
+  //     .then((res) => res.text())
+  //     .then((text) => {
+  //       let formattedNewResponse = JSON.parse(text)
+  //       let imageInfo = formattedNewResponse.response.photos.items[0]
+  //       this.setState({
+  //         imageSrc: imageInfo.prefix + '36x36' + imageInfo.suffix
+  //       })
+  //     })
+  //   })
+  //   .catch((error) => console.log("Error: ", error))
+  // }
 
   setClicked(status) {
     this.setState({
--- src/Search.js
+++ src/Search.js
@@ -3,6 +3,29 @@ import { List, ListItem } from 'material-ui/List'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
+import Restaurant from 'material-ui/svg-icons/maps/restaurant'
+import LocalLibrary from 'material-ui/svg-icons/maps/local-library'
+import LocalCafe from 'material-ui/svg-icons/maps/local-cafe'
+import LocalGroceryStore from 'material-ui/svg-icons/maps/local-grocery-store'
+import BeachAccess from 'material-ui/svg-icons/places/beach-access'
+import Cake from 'material-ui/svg-icons/social/cake'
+import School from 'material-ui/svg-icons/social/school'
+
+// import SvgIcon from 'material-ui/SvgIcon'
+
+// import Icon from 'material-ui/Icon'
+
+// import ActionInfo from 'material-ui/svg-icons/action/info'
+
+// function HomeIcon(props) {
+//   return (
+//     <svg style={{width: 20, height: 20 }} viewBox="0 0 20 20">
+//       <path fill="#000000" d="M12,20L12.76,17C9.5,16.79 6.59,15.4 5.75,13.58C5.66,14.06 5.53,14.5 5.33,14.83C4.67,16 3.33,16 2,16C3.1,16 3.5,14.43 3.5,12.5C3.5,10.57 3.1,9 2,9C3.33,9 4.67,9 5.33,10.17C5.53,10.5 5.66,10.94 5.75,11.42C6.4,10 8.32,8.85 10.66,8.32L9,5C11,5 13,5 14.33,5.67C15.46,6.23 16.11,7.27 16.69,8.38C19.61,9.08 22,10.66 22,12.5C22,14.38 19.5,16 16.5,16.66C15.67,17.76 14.86,18.78 14.17,19.33C13.33,20 12.67,20 12,20M17,11A1,1 0 0,0 16,12A1,1 0 0,0 17,13A1,1 0 0,0 18,12A1,1 0 0,0 17,11Z" />
+//     </svg>
+//   );
+// }
+
+
 class Search extends Component {
   state = {
     query: '',
@@ -51,21 +74,41 @@ class Search extends Component {
           onChange={(event) => this.updateQuery(event.target.value)}
         />
         <List style={{ maxHeight: '91%', overflow: 'auto' }}>
-          {showingPlaces.map((place) => (
-            <ListItem
-              // classes={ 'list-color' }
-              key={place.title}
-              // hoverColor='#62f442'
-              primaryText={place.title}
-              onClick={() => {
-                this.props.setCurrentPlace(place.title)
-                this.props.setClicked(true)
-              }}
-              // style={{ color: 'white', backgroundColor: '#44d6c7' }}
-              // onMouseEnter={(e) => e.target.style.backgroundColor = '#495054'}
-              // onMouseLeave={(e) => e.target.style.backgroundColor = '#44d6c7'}
-            />
-          ))}
+          {showingPlaces.map((place) => {
+            let optionalIcon
+            if (place.placeType === 'restaurant') {
+              optionalIcon = <Restaurant />
+            } else if (place.placeType === 'library') {
+              optionalIcon = <LocalLibrary />
+            } else if (place.placeType === 'cafe') {
+              optionalIcon = <LocalCafe />
+            } else if (place.placeType === 'grocery store') {
+              optionalIcon = <LocalGroceryStore />
+            } else if (place.placeType === 'beach') {
+              optionalIcon = <BeachAccess />
+            } else if (place.placeType === 'cake') {
+              optionalIcon = <Cake />
+            } else if (place.placeType === 'school') {
+              optionalIcon = <School />
+            }
+            return (
+              <ListItem
+                // classes={ 'list-color' }
+                key={place.title}
+                // hoverColor='#62f442'
+                primaryText={place.title}
+                rightIcon={optionalIcon}
+                // rightIcon={<HomeIcon />}
+                onClick={() => {
+                  this.props.setCurrentPlace(place.title)
+                  this.props.setClicked(true)
+                }}
+                // style={{ color: 'white', backgroundColor: '#44d6c7' }}
+                // onMouseEnter={(e) => e.target.style.backgroundColor = '#495054'}
+                // onMouseLeave={(e) => e.target.style.backgroundColor = '#44d6c7'}
+              />
+            )
+          })}
         </List>
       </div>
     )
